2921f4066a1f10cd7ad883a941fb015435b34f95
PirieD704 <dp75975@gmail.com>
2016-07-22 10:46:01 -0400

    update on array creation

--- scripts.js
+++ scripts.js
@@ -3,7 +3,34 @@
 //N Size Version
 var whosTurn = 1; //start off on player 1's turn
 
+var alph = ['a','b','c','d','e','f','g','h','i'];
 var winners = [];
+var gridSize = 5;
+
+// a0,a1,a2,a3,a4,...aN
+// b0,b1,b2,b3,b4,...bN
+
+var diag1 = [];
+var diag2 = [];
+
+
+for (var i = 0; i < gridSize; i++){
+	diag.push(alph[i] + i);
+	diag.push(alph[i] + (gridSize-i))
+	var winnersInsideH = [];
+	var winnersInsideV= [];
+	for (var j = 0; j < gridSize; j++){
+		//Vertical Winners
+		winnersInsideV.push(alph[j] + i);
+		//Horizontal Winners
+		winnersInsideH.push(alph[i] + j);	
+	}
+	winners.push(winnersInsideH);
+	winners.push(winnersInsideV);
+}
+
+winners.push(diag1);
+winner.push(diag2);
 
 // 1. Build a winners array
 // 2. We need to populate the board
